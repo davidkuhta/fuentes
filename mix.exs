19a0430075ef0f8c2b8c19c0a8d2097cc0e48482
David Kuhta <davidkuhta@gmail.com>
2016-05-25 23:14:11 -0500

    Added proper ecto dependency

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Fuentes.Mixfile do
 
   defp deps do
     [
-      {:ecto, git: "https://github.com/elixir-lang/ecto", branch: "master"},
+      {:ecto, "~> 2.0.0-rc.5"},
       {:postgrex, ">= 0.0.0"},
       {:ex_machina, git: "https://github.com/thoughtbot/ex_machina", branch: "master", only: :test},
       {:earmark, ">= 0.0.0", only: :dev},
